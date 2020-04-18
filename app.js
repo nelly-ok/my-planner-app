063e4d96d276859444f7435e9119eccb1a3af348
Nelson Okunlola <nelly@nellys-tool.local>
2020-04-18 02:52:44 -0500

    Add gitignoree, procfile, update

--- app.js
+++ app.js
@@ -35,9 +35,14 @@ app.post("/delete", function(req, res){
 });
 */
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Server started on port 3000.");
+app.listen(port, function(){
+  console.log("Server started successfully");
 });
 
 
